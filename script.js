1defd1e368eeb292aa6dbfd9cd8c52dbeb31a563
guntandrey <guntandrey@gmail.com>
2024-02-29 17:55:36 +0300

    good result, not scroll

---
+++ script.js
@@ -0,0 +1,6 @@
+function scrollDown() {
+    window.scrollTo({top: window.innerHeight, behavior: 'smooth' });
+}
+
+let button = document.querySelector('.header_btn')
+button.addEventListener('click', scrollDown)
